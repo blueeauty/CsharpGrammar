334b54f254fc63322833f0c28a2440c71167c6c7
blueeauty <115804854+blueeauty@users.noreply.github.com>
2024-12-26 14:24:47 +0800

    Initial commit

---
+++ CsharpGrammar/Program.cs
@@ -0,0 +1,85 @@
+﻿/************************************************
+*
+* FileName:	Program.cs
+* Description:  Csharp Grammar from Version 1.0 to Version 13
+*
+* Version:
+* Author: 	Administrator
+* CreateTime:	2024/12/26 14:13
+************************************************/
+
+#region Csharp1.0
+/// <summary>
+/// Tests the specified string.
+/// </summary>
+/// <param name="str">The string.</param>
+/// <returns></returns>
+string Test(string str)
+{
+    return string.Empty;
+}
+#endregion
+
+#region Csharp1.2
+
+#endregion
+
+#region Csharp2.0
+
+#endregion
+
+#region Csharp3.0
+
+#endregion
+
+#region Csharp4.0
+
+#endregion
+
+#region Csharp5.0
+
+#endregion
+
+#region Csharp6.0
+
+#endregion
+
+#region Csharp7.0
+
+#endregion
+
+#region Csharp7.1
+
+#endregion
+
+#region Csharp7.2
+
+#endregion
+
+#region Csharp7.3
+
+#endregion
+
+#region Csharp8.0
+
+#endregion
+
+#region Csharp9
+
+#endregion
+
+#region Csharp10
+
+#endregion
+
+#region Csharp11
+
+#endregion
+
+#region Csharp12
+
+#endregion
+
+#region Csharp13
+
+#endregion
