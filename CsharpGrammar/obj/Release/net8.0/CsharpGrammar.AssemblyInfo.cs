b058fdad22abd4e165da6f86227426045452af0e
blueeauty <115804854+blueeauty@users.noreply.github.com>
2024-12-26 14:28:15 +0800

    Initial update

--- CsharpGrammar/obj/Release/net8.0/CsharpGrammar.AssemblyInfo.cs
+++ CsharpGrammar/obj/Release/net8.0/CsharpGrammar.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsharpGrammar")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+334b54f254fc63322833f0c28a2440c71167c6c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsharpGrammar")]
