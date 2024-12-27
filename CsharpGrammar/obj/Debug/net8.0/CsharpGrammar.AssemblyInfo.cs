83f975997afbaa6bb7b641a714981ef93e379b8d
blueeauty <mahabubmastardsaer@gmail.com>
2024-12-27 13:52:51 +0800

    123123

--- CsharpGrammar/obj/Debug/net8.0/CsharpGrammar.AssemblyInfo.cs
+++ CsharpGrammar/obj/Debug/net8.0/CsharpGrammar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsharpGrammar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df50c5f072c12bd6810d42db25193dfb7cc7d15e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f571340b9645461ef6181e39c1a2a2162134555")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsharpGrammar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsharpGrammar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
